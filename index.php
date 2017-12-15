74e3290fe01005d92b6e98065e44e092efbcd637
armelle kalou <armellekalou25@gmail.com>
2017-12-15 23:30:10 +0000

    debut du projet: fin du formulaire

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exo</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="content">
+		<div>
+		<p>Bonjour chere codeuses vous pouvez vous faire recencer en vous inscrivant maintenant</p>
+	</div>
+	<div>
+		<fieldset>
+<form action="" name="form1" method="Post">
+	<table>
+		<tr>
+			<td>nom</td>
+			<td>:</td>
+			<td><input type="text" name="nom" value=""><br></td>
+		</tr>
+		<tr>
+			<td>prenom</td>
+			<td>:</td>
+			<td> <input type="text" name="prenom" value=""><br></td>
+		</tr>
+		<tr>
+			<td>tel</td>
+			<td>:</td>
+			<td><input type="text" name="tel" value=""><br></td>
+		</tr>
+		<tr>
+			<td>email</td>
+			<td>:</td>
+			<td><input type="text" name="email" value=""><br></td>
+		</tr>
+		<tr>
+			<td>description </td>
+			<td>:</td>
+			<td><br><textarea name="description"></textarea><br></td>
+		</tr>
+		<tr>
+			<td><input type="submit" value="enregistrer" name="submit"></td>
+		</tr>
+	</table>
+		</form>
+</fieldset>
+	</div>
+
+	</div>
+</body>
+</html>
